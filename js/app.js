ff9149f16f0700a7da5f701399714190d7c8c61f
Wolf <d.wolftripp@gmail.com>
2022-02-10 06:51:48 -0800

    psudocode

---
+++ js/app.js
@@ -0,0 +1,43 @@
+//add a data.js for all of the convorsations to clean up the doc
+
+//main screen 
+  //will be centered, not full page.
+  //should be able to see a start button, the title of the game, and a background
+
+//mechanics
+  //3 buttons will always be visable while the game is running.
+    //upon death they will be removed
+      //replaced by a restart button
+
+
+// pannel 1
+  // into duction to the game as
+
+// pannel 2
+  //
+
+// pannel 3
+  //
+  
+// pannel 4
+
+// pannel 5
+
+// pannel 6
+
+// pannel 7
+
+// pannel 8
+
+// pannel 9
+
+// pannel 10
+
+// pannel 11
+
+// pannel 12
+
+// death screens will show where your soul ended up. devoured or purgatory depending on where you died.
+
+//End (you Win)
+  //small bit about death letting you go.
