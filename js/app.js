19bb4d2696a0aade179cbbab60787b0865099985
Wolf <d.wolftripp@gmail.com>
2022-02-11 11:52:05 -0800

    add continue button

--- js/app.js
+++ js/app.js
@@ -8,26 +8,34 @@ let playerAlive
 const startBtn = document.querySelector('#start-button')
 const opBtn = document.querySelectorAll(".option-button")
 const resetBtn = document.querySelector("#reset-button")
+const contBtn = document.querySelector("#continue-button")
 
 /*----------------------------- Event Listeners -----------------------------*/
 startBtn.addEventListener("click", init)
-
+resetBtn.addEventListener("click", init)
 
 /*-------------------------------- Functions --------------------------------*/
 
 function init() {
   startBtn.setAttribute("hidden", true)
   playerAlive = true
-  pState()
+  opBtn[0].setAttribute("hidden", true)
+  opBtn[1].setAttribute("hidden", true)
+  opBtn[2].setAttribute("hidden", true)
+  contBtn.removeAttribute("hidden")
+  //call first pannel
 }
 
+//death pannels will call pState and make the playerAlive be false
 function pState() {
+
   if (playerAlive !== true) {
     opBtn[0].setAttribute("hidden", true)
     opBtn[1].setAttribute("hidden", true)
     opBtn[2].setAttribute("hidden", true)
     resetBtn.removeAttribute("hidden")
   }
+
 }
 
 
--- js/data.js
+++ js/data.js
@@ -1,1 +1,3 @@
 //add a data.js for all of the convorsations to clean up the doc
+
+//1. "welcome to hell! Almost, we are going to play a little game before I send you off."
