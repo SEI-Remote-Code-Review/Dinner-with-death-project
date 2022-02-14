4f1b5a4f91477386075319234570cdedb507b440
Wolf <d.wolftripp@gmail.com>
2022-02-13 17:03:42 -0800

    add dialog array

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
 /*---------------------------- Variables (state) ----------------------------*/
 let playerAlive
 
@@ -23,8 +22,13 @@ function init() {
   opBtn[1].setAttribute("hidden", true)
   opBtn[2].setAttribute("hidden", true)
   contBtn.removeAttribute("hidden")
-  //call first pannel
+  callOne()
 }
+
+
+
+
+
 
 //death pannels will call pState and make the playerAlive be false
 function pState() {
@@ -40,44 +44,9 @@ function pState() {
 
 
 
-//main screen 
-  //// will be centered, not full page.
-  ////should be able to see a start button, the title of the game, and a background
 
 
 
-//mechanics
-  ////3 buttons will always be visable while the game is running.
-    ////upon death they will be removed
-      ////replaced by a restart button
-
-
-// pannel 1
-  // into duction to the game as
-
-// pannel 2
-  //
-
-// pannel 3
-  //
-  
-// pannel 4
-
-// pannel 5
-
-// pannel 6
-
-// pannel 7
-
-// pannel 8
-
-// pannel 9
-
-// pannel 10
-
-// pannel 11
-
-// pannel 12
 
 // death screens will show where your soul ended up. devoured or purgatory depending on where you died.
 
