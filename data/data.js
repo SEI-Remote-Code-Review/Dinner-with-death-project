4c64545d2a7b3b76b3cecbe03ce767107f7cf4bc
Wolf <d.wolftripp@gmail.com>
2022-02-14 11:48:26 -0800

    add choice buttons functionality

--- data/data.js
+++ data/data.js
@@ -1,4 +1,29 @@
-const pannelQuotes = ["welcome to hell! Almost, we are going to play a little game before I send you off.", "The game is simple. I'll provide you with a choice of two dishes, you just need to pick the right one.", "youre first set of choices, beans. Or steak seared in a garlic aioli, with buttered mashed potats, DRENTCHED in a gravy sauce found in the seventh layer of hell. whats your choice.", "you chose wrong kid. but on the bright side, im FAMISHED.", "id roll my eyes... IF I HAD SOME! how about we go again?", "second round! we have a delicous looking pork roast served with brussle sprouts sautayed with bacon and oions drizzled with a carmalized brown sugar sauce. Or a tofu tacos.", "be honest with me, did my secritary tell you the answers to these questions? I fired Janus for doing the same thing...", "well here we are, the last round. Guess this one right as ill send you back to where ever you came from. But this ones going to be a doozy!", "a delectable dozen chewy pumpkin oatmeal cookies, vegan of corse. Or butter cake with vanilla ice cream", "ya got me kid, im against animal cruility. Ill send you back."]
+const pannelQuotes = [
+  //array includes text and isChoice 
+  ["welcome to hell! Almost, we are going to play a little game before I send you off.", false],
+
+  ["The game is simple. I'll provide you with a choice of two dishes, you just need to pick the right one.", false],
+
+  ["youre first set of choices, beans. Or steak seared in a garlic aioli, with buttered mashed potats, DRENTCHED in a gravy sauce found in the seventh layer of hell. whats your choice.", true],
+
+  ["you chose wrong kid. but on the bright side, im FAMISHED.", false],
+
+  ["id roll my eyes... IF I HAD SOME! how about we go again?", false],
+
+  [  "second round! we have a delicous looking pork roast served with brussle sprouts sautayed with bacon and oions drizzled with a carmalized brown sugar sauce. Or a tofu tacos.", true],
+
+  ["you chose wrong kid. but on the bright side, im FAMISHED.", false],
+
+  ["be honest with me, did my secritary tell you the answers to these questions? I fired Janus for doing the same thing...", false],
+
+  [  "well here we are, the last round. Guess this one right as ill send you back to where ever you came from. But this ones going to be a doozy!", false],
+
+  [  "a delectable dozen chewy pumpkin oatmeal cookies, vegan of corse. Or butter cake with vanilla ice cream", true],
+
+  ["you chose wrong kid. but on the bright side, im FAMISHED.", false],
+  
+  [  "ya got me kid, im against animal cruility. Ill send you back.", false]
+]
 
 export {
   pannelQuotes
--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ let currentPage = 0
 /*------------------------ Cached Element References ------------------------*/
 const startBtn = document.querySelector('#start-button')
 const opBtn = document.querySelectorAll(".option-button")
+const opBtn1 = document.getElementById("option1")
+const opBtn2 = document.getElementById("option2")
 const resetBtn = document.querySelector("#reset-button")
 const contBtn = document.querySelector("#continue-button")
 const dialog = document.querySelector(".dialog-box")
@@ -17,11 +19,13 @@ contBtn.addEventListener("click", nextPage)
 /*-------------------------------- Functions --------------------------------*/
 
 function init() {
+  contBtn.removeAttribute("hidden")
   startBtn.setAttribute("hidden", true)
-  playerAlive = false
   opBtn[0].setAttribute("hidden", true)
   opBtn[1].setAttribute("hidden", true)
-  contBtn.removeAttribute("hidden")
+  resetBtn.setAttribute("hidden", true)
+  playerAlive = true
+  currentPage = 0
   callOne()
 }
 
@@ -35,37 +39,48 @@ function callOne () {
   choiceOne()
 }
 
+opBtn1.addEventListener("click", function(){
+  currentPage += 2
+  
+  callOne()
+})
 
+opBtn2.addEventListener("click", function(){
+  currentPage += 1
+  playerAlive = false
+  callOne()
+})
 
+function choiceOne() {
+  if (playerAlive !== true) {
+    opBtn[0].setAttribute("hidden", true)
+    opBtn[1].setAttribute("hidden", true)
+    resetBtn.removeAttribute("hidden")
+  } else {
+    if (pannelQuotes[currentPage][1] === true){
+      opBtn[0].removeAttribute("hidden", true)
+      opBtn[1].removeAttribute("hidden", true)
+      contBtn.setAttribute("hidden", true)
+    } else {
+      opBtn[0].setAttribute("hidden", true)
+      opBtn[1].setAttribute("hidden", true)
+      contBtn.removeAttribute("hidden", true)
+    }
+  }
+  choiceOneBranch()
+}
 
+function choiceOneBranch() {
+//   if (opBtn[0] === click) {
+//     dialog.textContent = `${pannelQuotes[3]}`
+//   } else {
+//     dialog.textContent = `${pannelQuotes[4]}`
+//   }
+
+}
 
 //if statement for when we reach idx 3 
   //continue button is hidden and option buttons are un hidden
 //function to decide what index to show after the option button is clicked
 //continue button will come back until another choice
 
-
-
-
-//death pannels will call pState and make the playerAlive be false
-function pState() {
-
-  if (playerAlive !== true) {
-    opBtn[0].setAttribute("hidden", true)
-    opBtn[1].setAttribute("hidden", true)
-    opBtn[2].setAttribute("hidden", true)
-    resetBtn.removeAttribute("hidden")
-  }
-
-}
-
-
-
-
-
-
-
-// death screens will show where your soul ended up. devoured or purgatory depending on where you died.
-
-//End (you Win)
-  //small bit about death letting you go.
