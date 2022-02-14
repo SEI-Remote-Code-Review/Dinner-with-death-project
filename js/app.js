b756364be3eeca023984fdec99d1e445fac0e2fe
Wolf <d.wolftripp@gmail.com>
2022-02-14 10:52:01 -0800

    add continue button functionality

--- js/app.js
+++ js/app.js
@@ -3,30 +3,46 @@ import { pannelQuotes } from "../data/data.js"
 
 /*---------------------------- Variables (state) ----------------------------*/
 let playerAlive
-
+let currentPage = 0
 /*------------------------ Cached Element References ------------------------*/
 const startBtn = document.querySelector('#start-button')
 const opBtn = document.querySelectorAll(".option-button")
 const resetBtn = document.querySelector("#reset-button")
 const contBtn = document.querySelector("#continue-button")
-
+const dialog = document.querySelector(".dialog-box")
 /*----------------------------- Event Listeners -----------------------------*/
 startBtn.addEventListener("click", init)
 resetBtn.addEventListener("click", init)
-
+contBtn.addEventListener("click", nextPage)
 /*-------------------------------- Functions --------------------------------*/
 
 function init() {
   startBtn.setAttribute("hidden", true)
-  playerAlive = true
+  playerAlive = false
   opBtn[0].setAttribute("hidden", true)
   opBtn[1].setAttribute("hidden", true)
-  opBtn[2].setAttribute("hidden", true)
   contBtn.removeAttribute("hidden")
   callOne()
 }
 
+function nextPage() {
+  currentPage += 1
+  callOne()
+}
 
+function callOne () {
+  dialog.textContent = `${pannelQuotes[currentPage]}`
+  choiceOne()
+}
+
+
+
+
+
+//if statement for when we reach idx 3 
+  //continue button is hidden and option buttons are un hidden
+//function to decide what index to show after the option button is clicked
+//continue button will come back until another choice
 
 
 
