bf5be9c5486f47c312ab7be90620cd2b4e47da45
Wolf <d.wolftripp@gmail.com>
2022-02-15 09:32:50 -0800

    add death animations and bacground files

--- data/data.js
+++ data/data.js
@@ -28,35 +28,4 @@ const pannelQuotes = [
 export {
   pannelQuotes
 }
-
-// 1. "welcome to hell! Almost, we are going to play a little game before I send you off."
-
-//2. "The game is simple. I'll provide you with a choice of two dishes, you just need to pick the right one."
-
-//3. "youre first set of choices, beans. Or steak seared in a garlic aioli, with buttered mashed potats, DRENTCHED in a gravy sauce found in the seventh layer of hell. whats your choice.""
-
-//4. choice 2
-  //"you chose wrong kid. but on the bright side, im FAMISHED."
-
-//5. choice 1
-  // "id roll my eyes... IF I HAD SOME! how about we go again?""
-
-//6. "second round! we have a delicous looking pork roast served with brussle sprouts sautayed with bacon and oions drizzled with a carmalized brown sugar sauce. Or a tofu tacos."
-
-//7. choice 1
-  //"you chose wrong kid. but on the bright side, im FAMISHED."
-
-//8. choice 2
-  //"be honest with me, did my secritary tell you the answers to these questions? I fired Janus for doing the same thing..."
-
-//9. "well here we are, the last round. Guess this one right as ill send you back to where ever you came from. But this ones going to be a doozy!"
-
-//10. "a delectable dozen chewy pumpkin oatmeal cookies, vegan of corse. Or butter cake with vanilla ice cream"
-
-//11. choice 2
-  //"you chose wrong kid. but on the bright side, im FAMISHED."
-
-//12. choice 1
-  //"ya got me kid, im against animal cruility. Ill send you back."
-
   
